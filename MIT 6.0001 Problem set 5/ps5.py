ec0c52d85119d20c49e3b579b041dded3b91177b
Simo Idrissi <elfellah.med@gmail.com>
2023-06-05 11:53:29 +0200

    Problem 8 & 9

--- MIT 6.0001 Problem set 5/ps5.py
+++ MIT 6.0001 Problem set 5/ps5.py
@@ -201,10 +201,23 @@ class NotTrigger(Trigger):
 
 # Problem 8
 # TODO: AndTrigger
+class AndTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
 
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
+    
 # Problem 9
 # TODO: OrTrigger
+class OrTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
 
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) or self.trigger2.evaluate(story)
 
 #======================
 # Filtering
